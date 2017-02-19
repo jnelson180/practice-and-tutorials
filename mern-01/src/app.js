05244c4529cef7fd857584bb43eeaecff9daa9a8
Jake Nelson <jnelson180@gmail.com>
2017-02-18 23:01:33 -0600

    Added differences text file to main directory to track specific changes I am making throughout the tutorial

--- mern-01/src/app.js
+++ mern-01/src/app.js
@@ -2,10 +2,52 @@ class BugList extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     return (
-      <div>Placeholder text</div>
+      <div>
+      <div>Bug list loads in here.</div>
+      <div>
+        <h1>Bug Tracker</h1>
+        <BugFilter />
+        <hr />
+        <BugTable />
+        <hr />
+        <BugAdd />
+      </div>
+      </div>
+    );
+  }
+}
+
+class BugFilter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Filter bugs in this section.</div>
+    );
+  }
+}
+
+class BugTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>This will be a filtered list / table of the bugs.</div>
+    );
+  }
+}
+
+class BugAdd extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>Use this form to add a bug.</div>
     );
   }
 }
--- mern-01/static/app.js
+++ mern-01/static/app.js
@@ -2,12 +2,68 @@ class BugList extends React.Component {
   constructor(props) {
     super(props);
   }
-
   render() {
     return React.createElement(
       'div',
       null,
-      'Placeholder text'
+      React.createElement(
+        'div',
+        null,
+        'Bug list loads in here.'
+      ),
+      React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'h1',
+          null,
+          'Bug Tracker'
+        ),
+        React.createElement(BugFilter, null),
+        React.createElement('hr', null),
+        React.createElement(BugTable, null),
+        React.createElement('hr', null),
+        React.createElement(BugAdd, null)
+      )
+    );
+  }
+}
+
+class BugFilter extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Filter bugs in this section.'
+    );
+  }
+}
+
+class BugTable extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'This will be a filtered list / table of the bugs.'
+    );
+  }
+}
+
+class BugAdd extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return React.createElement(
+      'div',
+      null,
+      'Use this form to add a bug.'
     );
   }
 }
