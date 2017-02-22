b6e956e03873acde31e6215219864ce2e71053c0
Jake Nelson <jnelson180@gmail.com>
2017-02-21 21:19:35 -0600

    Finished 5.2, pre comparison to vasansrs diffs

--- mern-01/webapp.js
+++ mern-01/webapp.js
@@ -1,12 +1,15 @@
 var express = require('express')
 var app = express()
+var bodyParser = require('body-parser')
 
-app.use(express.static('static'));
+app.use(express.static('static'))
 
 var bugData = [
   {id: 1, priority: 'P1', status:'Open', owner:'Ravan', title:'App crashes on open'},
   {id: 2, priority: 'P2', status: 'New', owner:'Eddie', title:'Misaligned border on panel'},
 ];
+
+var jsonParser = bodyParser.json({ "type": "application/json" });
 
 app.use(express.static('static'))
 
@@ -14,7 +17,28 @@ app.get('/api/bugs', function (req, res) {
   res.status(200).send(JSON.stringify(bugData));
 })
 
+app.post('/api/bugs', jsonParser, function (req, res) {
+  console.log('Received new bug: ');
+  let newBug = {
+    id: bugData.length + 1,
+    priority: req.body.priority,
+    status: req.body.status,
+    owner: req.body.owner,
+    title: req.body.title
+  }
+  newBugData = bugData.slice(0);
+  newBugData.push(newBug);
+  bugData = newBugData;
+  console.log(bugData);
+  res.json(newBug);
+})
+
 var server = app.listen(3000, function() {
   var port = server.address().port;
   console.log("Server listening on port " + port + "!");
 })
+
+
+/*
+curl -H "Content-Type: application/json" -X POST -d '{"priority":"P2", "status":"New", "owner":"Jake", "title":"Test bug"}' http://localhost:3000/api/bugs
+*/
